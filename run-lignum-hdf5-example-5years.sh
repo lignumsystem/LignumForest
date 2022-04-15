0dbe8a0725a959f0c197571567056ac9957abe6a
jariperttunen <jari.perttunen@luke.fi>
2022-04-15 16:37:35 +0300

    Renamed due to type in .sh suffix

---
+++ run-lignum-hdf5-example-5years.sh
@@ -0,0 +1,1 @@
+./lig-forest -iter 5 -metafile MetaFile.txt -hdf5 HDF5ForestData1.h5 -voxelspace VoxelSpace.txt   -numParts 2  -toFile results/this41.dat -generateLocations 250 -dumpSelf -hw 15 -n_buds_ini_min 3 -n_buds_ini_max 3 -increaseXi 15 -noBorderForest -p0Var 20 -xml results/this41.xml  > results/now23.dat
