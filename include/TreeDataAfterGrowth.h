47436d77720b55ecea339fd33c11e3e76e1d7b4e
Risto Sievänen <rpsievan@gmail.com>
2022-04-18 16:06:23 +0300

    I changed bullet points to a numbered list in Doxygen documentation.

--- include/TreeDataAfterGrowth.h
+++ include/TreeDataAfterGrowth.h
@@ -47,57 +47,57 @@ const array<string,51> tdcn={
   "MeanBranch_SumD^2","MeanBranch_SumL","MeanBranch_SumD^2*L","MeanBranch_SumD^2*L/SumD^2","MeanBranch_Nbranch","MeanBranch_SumL/Nbranch","lambda"
 };
 /// Column names for the HDF5 file tree data.
-/// + TreeId: Unique number of the tree in the Forest
-/// + X:  The x,y,z coordinates define the location of the tree
-/// + Y:  y xoordinate
-/// +  Z:  z coordinate
-/// +  TreeNseg: Number of segments in a tree
-/// +  TreeCrownVol: Crown volume \sa CrownVolume
-/// +  TreeH: Tree height
-/// +  TreeDBase: Tree base diameter
-/// +  TreeDbh: Tree diameter breast height (D 1.3m)
-/// +  TreeDCrownBase: Tree diameter crown base
-/// +  TreeHCrownBase: Tree crown base height
-/// +  TreeAsBase: Sapwood area at base
-/// +  TreeAsDbh: Sapwood area Dbh (D1.3 m)
-/// +  TreeAsCrownBase: Sappwood area at crown base
-/// +  TreeAf: Foliage area in a tree
-/// +  AxisVol: Main axis volume \sa MainAxisVolume
-/// +  TreeP: Tree photosynthesis (*before* new growth tree segments)
-/// +  TreeM: Tree respiration (*before* new growth)
-/// +  Mr_prev: Root respiration *before* new growth
-/// +  M_above: Above ground respiration: TreeM-Mr_prev
-/// +  Ms: Sapwood respiration (*before* new growth)
-/// +  Mf: Folaige respiration (*before* new growth)
-/// +  Wf: Tree foliage mass
-/// +  Wf_new: Foliage mass in *new* segments
-/// +  Ws: All sapwood mass (Ws_new+Ws_old)
-/// +  Ws_old: Sapwood mass in *old* segments after growth
-/// +  Ws_D_growth: Sapwood mass for new diameter growth: Ws_old - sapwood_after_senescense
-/// +  Ws_new: Sapwood in *new* segments.
-/// +  Ws_D_growth+Ws_new: Sapwood in growth
-/// +  Wr: Root mass
-/// +  Wr_new: New root mass (required by foliage)
-/// +  QinTop: Incoming radiation at the top of the tree
-/// +  QinMax: Max Qin in the forest
-/// +  QinTop/QinMax: Relative Qin for the tree 
-/// +  Qabs: Absorbed radiation in a tree
-/// +  Qabs/(DiffBallSensor*TreeAf): Radiation efficiency
-/// +  Wf_P: foliage that photosynthesised
-/// +  TreeP/Wf_P: P/Wf ratio
-/// +  ASeg0: Surface area of new segments
-/// +  W: Tree Wood mass (sapwood+heartwood)
-/// +  Wstem: Wood mass main axis
-/// +  Wbranch: Wood mass in branches (W-Wstem)
-/// +  Ws_stem: Sapwood in main axis
-/// +  Nsegment: Number of tree segments in a tree
-/// +  MeanBranch_SumD^2: Sum of branch diameters squared (main axis branches only)
-/// +  MeanBranch_SumL: Sum of branch lengths (branch length: sum of segment lengths in the main axis of a branch)
-/// +  MeanBranch_SumD^2*L: Sum of each branch D^2*L
-/// +  MeanBranch_SumD^2*L/SumD^2: Mean branch length (Branch lengths weighted by D^2)
-/// +  MeanBranch_Nbranch: Sum of branches in the main axis.
-/// +  MeanBranch_SumL/Nbranch: Mean branch length (SumL/Nbranch)
-/// +  lambda: Lambda s.t. P-M=G(lambda)
+/// -# TreeId:   Unique number of the tree in the Forest
+/// -# X:   The x,y,z coordinates define the location of the tree
+/// -# Y:   y xoordinate
+/// -#  Z:   z coordinate
+/// -#  TreeNseg:   Number of segments in a tree
+/// -#  TreeCrownVol:   Crown volume \sa CrownVolume
+/// -#  TreeH:   Tree height
+/// -#  TreeDBase:   Tree base diameter
+/// -#  TreeDbh:   Tree diameter breast height (D 1.3m)
+/// -#  TreeDCrownBase:   Tree diameter crown base
+/// -#  TreeHCrownBase:   Tree crown base height
+/// -#  TreeAsBase:   Sapwood area at base
+/// -#  TreeAsDbh:   Sapwood area Dbh (D1.3 m)
+/// -#  TreeAsCrownBase:   Sappwood area at crown base
+/// -#  TreeAf:   Foliage area in a tree
+/// -#  AxisVol:   Main axis volume \sa MainAxisVolume
+/// -#  TreeP:   Tree photosynthesis (*before* new growth tree segments)
+/// -#  TreeM:   Tree respiration (*before* new growth)
+/// -#  Mr_prev:   Root respiration *before* new growth
+/// -#  M_above:   Above ground respiration:   TreeM-Mr_prev
+/// -#  Ms:   Sapwood respiration (*before* new growth)
+/// -#  Mf:   Folaige respiration (*before* new growth)
+/// -#  Wf:   Tree foliage mass
+/// -#  Wf_new:   Foliage mass in *new* segments
+/// -#  Ws:   All sapwood mass (Ws_new+Ws_old)
+/// -#  Ws_old:   Sapwood mass in *old* segments after growth
+/// -#  Ws_D_growth:   Sapwood mass for new diameter growth:   Ws_old - sapwood_after_senescense
+/// -#  Ws_new:   Sapwood in *new* segments.
+/// -#  Ws_D_growth+Ws_new:   Sapwood in growth
+/// -#  Wr:   Root mass
+/// -#  Wr_new:   New root mass (required by foliage)
+/// -#  QinTop:   Incoming radiation at the top of the tree
+/// -#  QinMax:   Max Qin in the forest
+/// -#  QinTop/QinMax:   Relative Qin for the tree 
+/// -#  Qabs:   Absorbed radiation in a tree
+/// -#  Qabs/(DiffBallSensor*TreeAf):   Radiation efficiency
+/// -#  Wf_P:   foliage that photosynthesised
+/// -#  TreeP/Wf_P:   P/Wf ratio
+/// -#  ASeg0:   Surface area of new segments
+/// -#  W:   Tree Wood mass (sapwood+heartwood)
+/// -#  Wstem:   Wood mass main axis
+/// -#  Wbranch:   Wood mass in branches (W-Wstem)
+/// -#  Ws_stem:   Sapwood in main axis
+/// -#  Nsegment:   Number of tree segments in a tree
+/// -#  MeanBranch_SumD^2:   Sum of branch diameters squared (main axis branches only)
+/// -#  MeanBranch_SumL:   Sum of branch lengths (branch length:   sum of segment lengths in the main axis of a branch)
+/// -#  MeanBranch_SumD^2*L:   Sum of each branch D^2*L
+/// -#  MeanBranch_SumD^2*L/SumD^2:   Mean branch length (Branch lengths weighted by D^2)
+/// -#  MeanBranch_Nbranch:   Sum of branches in the main axis.
+/// -#  MeanBranch_SumL/Nbranch:   Mean branch length (SumL/Nbranch)
+/// -#  lambda:   Lambda s.t. P-M=G(lambda)
 /// \note Technically C++ standard defines vector initialization as with array.
 /// It seems not all compilers have implemented it yet. \sa tdcn.
 const vector<string> TREE_DATA_COLUMN_NAMES(tdcn.begin(),tdcn.end());
