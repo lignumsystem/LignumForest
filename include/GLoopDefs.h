c7dc83f1756dd5911b7d02c57b1f79c375979262
Jari Perttunen <jari.perttunen@luke.fi>
2024-05-21 15:04:55 +0300

    Pine L-system namespace and GrowthLoop typedef wrapped in header files. The main loop in lignum-forest.cc more readable.

---
+++ include/GLoopDefs.h
@@ -0,0 +1,17 @@
+#ifndef GLOOPDEFS_H
+#define GLOOPDEFS_H
+#include <lengine.h>
+#include <ScotsPine.h>
+#include <GrowthLoop.h>
+#include <pinelsystem.h>
+using namespace Pine;
+using namespace LignumForest;
+
+namespace LignumForest{
+  /// \typedef GrowthLoop<ScotsPineTree,ScotsPineSegment,ScotsPineBud, Pine::LSystem<ScotsPineSegment,ScotsPineBud,PBNAME,PineBudData> > ScotsPineForest
+  /// ScotsPineForest (i.e. GrowthLoop template instance) captures the growth loop of a forest stand.
+  typedef GrowthLoop<ScotsPineTree,ScotsPineSegment,ScotsPineBud,
+		     Pine::LSystem<ScotsPineSegment,ScotsPineBud,PBNAME,PineBudData> > ScotsPineForest;
+}
+
+#endif
---
+++ include/pinelsystem.h
@@ -0,0 +1,6 @@
+#ifndef PINELSYSTEM_H
+#define PINELSYSTEM_H
+namespace Pine{
+#include <LSystem.h>
+}
+#endif
