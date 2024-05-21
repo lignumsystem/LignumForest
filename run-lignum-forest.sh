07d5b7055cb066535ac8177f45516b5c25135c17
Jari Perttunen <jari.perttunen@luke.fi>
2024-05-21 15:15:54 +0300

    Sample command line for lignum-forest that has -modeChange and -architectureChange flags. The slurm script is for Slurm workload manager.

--- run-lignum-forest.sh
+++ run-lignum-forest.sh
@@ -1,4 +1,4 @@
 #CHECK: Have you done `make install` after compilation?
 #REMEMBER: Set -hdf5 <filename> !!!!!!!
 #NOTE: Protect regular expression for -metafile with quotes. 
-./lignum-forest -iter 60 -metafile 'MetaFile*.txt' -voxelspace VoxelSpace.txt  -modeChange 20 -architectureChange 10 -numParts 2 -hw 10 -kBorderConifer 0.11 -writeInterval 5 -increaseXi 15 -generateLocations 770 -treeDist 0.3 -hdf5 April172024Forest.h5  -dumpSelf  -n_buds_ini_min 3 -n_buds_ini_max 3  -verbose > runlog.txt
+./lignum-forest -iter 60 -metafile 'MetaFile*.txt' -voxelspace VoxelSpace40x40.txt  -modeChange 20 -architectureChange 10 -numParts 2 -hw 10 -kBorderConifer 0.11 -writeInterval 5 -increaseXi 15 -generateLocations 4000 -treeDist 0.3 -hdf5 May162024LignumForest.h5  -dumpSelf  -n_buds_ini_min 3 -n_buds_ini_max 3  -verbose > runlog.txt
