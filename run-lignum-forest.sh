82e0c7dd2a055d6e311a5b853d7f8af4996acd62
Risto Sievänen <rpsievan@gmail.com>
2025-10-24 20:19:26 +0300

    These seem to produce a good forest development.

--- run-lignum-forest.sh
+++ run-lignum-forest.sh
@@ -1,2 +1,2 @@
-./lignum-forest -iter 80 -metafile MetaFile.txt -voxelspace VoxelSpace.txt -architectureChange 10 -butt_swell_coeff 0.0015 -butt_swell_start 0 -numParts 2 -hw 5 -kBorderConifer 0.2 -writeInterval 20 -increaseXi 15 -fsapwdown fsapwdown.fun -generateLocations 770 -treeDist 0.3 -hdf5 koe23.h5 -dumpSelf -n_buds_ini_min 3 -n_buds_ini_max 3 -verbose -terminate_buds
+./lignum-forest -iter 80 -metafile MetaFile.txt -voxelspace VoxelSpace.txt -architectureChange 10 -butt_swell_coeff 0.0015 -butt_swell_start 0 -numParts 2 -hw 5 -kBorderConifer 0.2 -writeInterval 20 -increaseXi 15 -fsapwdown fsapwdown.fun -generateLocations 3000 -treeDist 0.3 -hdf5 koe39.h5 -dumpSelf -n_buds_ini_min 3 -n_buds_ini_max 3 -verbose -terminate_buds
 
